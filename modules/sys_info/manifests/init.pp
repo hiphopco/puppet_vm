dcb474745b572599821ee2f8b43a92e8791afde1
chonti <kms313@ssu.ac.kr>
2015-11-10 23:19:56 +0900

    First commit

---
+++ modules/sys_info/manifests/init.pp
@@ -0,0 +1,9 @@
+class sys_info{
+	package { 'gkrellm':
+		ensure => '2.3.5-5'
+	}
+	exec { 'gkrellm' :
+		command => '/usr/bin/gkrellm &',
+		require => Package['gkrellm'],
+	}
+}
---
+++ modules/vim_upgrade/files/.vimrc
@@ -0,0 +1,6 @@
+set number
+set ai
+set si
+set cindent
+set tabstop=4
+syntax on
